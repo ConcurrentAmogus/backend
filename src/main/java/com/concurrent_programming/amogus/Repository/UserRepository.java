ad2a92b33c2dbda8ea1e27b8807b27a186424d0b
yanshiuh <yanshiuh12372@gmail.com>
2023-05-12 22:34:19 +0800

    Change port to 8081

--- src/main/java/com/concurrent_programming/amogus/Repository/UserRepository.java
+++ src/main/java/com/concurrent_programming/amogus/Repository/UserRepository.java
@@ -1,15 +1,76 @@
 package com.concurrent_programming.amogus.Repository;
 
 import com.concurrent_programming.amogus.Model.User;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.*;
+import com.google.firebase.cloud.FirestoreClient;
+import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
-public interface UserRepository {
+@Repository
+public class UserRepository {
 
-    public List<User> findAll() throws ExecutionException, InterruptedException;
-    public User findById(String documentId) throws ExecutionException, InterruptedException;
-    public User create(User user);
-    public User update(User user) throws ExecutionException, InterruptedException;
-    public String delete(String documentId) throws ExecutionException, InterruptedException;
+    Firestore firestore = FirestoreClient.getFirestore();
+
+    public List<User> findAll() throws ExecutionException, InterruptedException {
+        ApiFuture<QuerySnapshot> apiFuture = firestore.collection("user").get();
+        List<QueryDocumentSnapshot> docs = apiFuture.get().getDocuments();
+        List<User> users = new ArrayList<>();
+        docs.forEach(doc -> {
+            users.add(doc.toObject(User.class));
+        });
+
+        return users;
+    }
+
+    public User findById(String id) throws ExecutionException, InterruptedException {
+        DocumentReference documentReference = firestore.collection("user").document(id);
+        ApiFuture<DocumentSnapshot> apiFuture = documentReference.get();
+        DocumentSnapshot doc = apiFuture.get();
+        User user;
+        if(doc.exists()) {
+            user = doc.toObject(User.class);
+            return user;
+        }
+
+        return null;
+    }
+
+    public User create(User user) {
+        if (user == null ||
+            user.getId().trim().equals("") ||
+            user.getUsername().trim().equals("")
+        ) {
+            return null;
+        }
+
+        ApiFuture<WriteResult> apiFuture = firestore.collection("user").document(user.getId()).set(user);
+        return user;
+    }
+
+    public User update(User user) throws ExecutionException, InterruptedException {
+        DocumentReference documentReference = firestore.collection("user").document(user.getId());
+        ApiFuture<DocumentSnapshot> apiFuture = documentReference.get();
+        DocumentSnapshot doc = apiFuture.get();
+        if(doc.exists()) {
+            ApiFuture<WriteResult> writeResultApiFuture = firestore.collection("user").document(user.getId()).set(user);
+            return user;
+        }
+        return null;
+    }
+
+    public String delete(String id) throws ExecutionException, InterruptedException {
+        DocumentReference documentReference = firestore.collection("user").document(id);
+        ApiFuture<DocumentSnapshot> apiFuture = documentReference.get();
+        DocumentSnapshot doc = apiFuture.get();
+        if(doc.exists()) {
+            ApiFuture<WriteResult> writeResultApiFuture = firestore.collection("user").document(id).delete();
+            return "Deleted";
+        }
+        return "";
+    }
+
 }
--- src/main/java/com/concurrent_programming/amogus/Service/UserService.java
+++ src/main/java/com/concurrent_programming/amogus/Service/UserService.java
@@ -1,20 +1,37 @@
 package com.concurrent_programming.amogus.Service;
 
 import com.concurrent_programming.amogus.Model.User;
+import com.concurrent_programming.amogus.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+@Service
+public class UserService {
 
-public interface UserService {
+    @Autowired
+    UserRepository userRepository;
 
-    public List<User> findAllUsers() throws ExecutionException, InterruptedException;
+    public List<User> findAllUsers() throws ExecutionException, InterruptedException {
+        return userRepository.findAll();
+    }
 
-    public User findUserById(String id) throws ExecutionException, InterruptedException;
+    public User findUserById(String id) throws ExecutionException, InterruptedException {
+        return userRepository.findById(id);
+    }
 
-    public User createUser(User user);
+    public User createUser(User user) {
+        return userRepository.create(user);
+    }
 
-    public User updateUser(User user) throws ExecutionException, InterruptedException;
+    public User updateUser(User user) throws ExecutionException, InterruptedException {
+        return userRepository.update(user);
+    }
 
-    public String deleteUser(String id) throws ExecutionException, InterruptedException;
+    public String deleteUser(String id) throws ExecutionException, InterruptedException {
+        return userRepository.delete(id);
+    }
+
 }
